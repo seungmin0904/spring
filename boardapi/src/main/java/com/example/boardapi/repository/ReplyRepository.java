45bfa9bd5ffeec429f7254f1bcd5e701820b975a
seungmin <gogo90490@gmail.com>
2025-05-15 18:00:35 +0900

    api 시작

--- boardapi/src/main/java/com/example/boardapi/repository/ReplyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.boardapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.boardapi.entity.reply;
-
-public interface ReplyRepository extends JpaRepository<reply, Long> {
-
-}
